0a531d30f1bfbac691ac991630f712b607cd238a
Kevin Andersen <k.andersen@lego.com>
2022-08-20 20:28:05 +0200

    Panelbear now installed as package. Custom tracking event added for export.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import Morphaweb from './Morphaweb'
- 
+import * as Panelbear from '@panelbear/panelbear-js'
+
 const morphaweb = new Morphaweb()
+
+/**
+ * Tracking
+ */
+
+Panelbear.load("z6xwVIhy6M");
+Panelbear.trackPageview();
+
+const exportButton = document.getElementById("export")
+exportButton.addEventListener("click", (e) => {
+    Panelbear.track("export")
+})
