ae94138b9ded6b7e411880a5dc05154728231be9
Kevin Andersen <k.andersen@lego.com>
2022-08-22 20:14:46 +0200

    overlay will appear when dragging file

--- src/DropHandler.js
+++ src/DropHandler.js
@@ -1,16 +1,20 @@
 export default class DropHandler {
     constructor(morphaweb) {
         this.morphaweb = morphaweb
-        document.addEventListener("dragover", this.allowDrop)
+        this.overlay = document.getElementById("overlay")
+        
+        document.addEventListener("dragover", this.allowDrop.bind(this))
         document.addEventListener("drop", this.onDrop.bind(this))
     }
 
     allowDrop(e) {
+        this.overlayShow()
         e.preventDefault()
     }
     
     onDrop(e) {
         e.preventDefault()
+        this.overlayHide()
         if(e.dataTransfer.files.length > 1) {
             console.log(e.dataTransfer.files)
         } else {
@@ -18,5 +22,13 @@ export default class DropHandler {
         }
         this.morphaweb.wavesurfer.loadBlob(this.morphaweb.activeFile)
     }
-    
+
+
+    overlayShow() {
+        this.overlay.style.display = 'block'
+    }
+
+    overlayHide() {
+        this.overlay.style.display = 'none'
+    }
 }
