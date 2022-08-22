52ee0cf0e316012218c1bbbc255aa8dd2d82e8a0
Kevin Andersen <k.andersen@lego.com>
2022-08-22 20:15:02 +0200

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -5642,17 +5642,26 @@ parcelHelpers.defineInteropFlag(exports);
 class DropHandler {
     constructor(morphaweb){
         this.morphaweb = morphaweb;
-        document.addEventListener("dragover", this.allowDrop);
+        this.overlay = document.getElementById("overlay");
+        document.addEventListener("dragover", this.allowDrop.bind(this));
         document.addEventListener("drop", this.onDrop.bind(this));
     }
     allowDrop(e) {
+        this.overlayShow();
         e.preventDefault();
     }
     onDrop(e) {
         e.preventDefault();
+        this.overlayHide();
         if (e.dataTransfer.files.length > 1) console.log(e.dataTransfer.files);
         else this.morphaweb.activeFile = e.dataTransfer.files[0];
         this.morphaweb.wavesurfer.loadBlob(this.morphaweb.activeFile);
+    }
+    overlayShow() {
+        this.overlay.style.display = "block";
+    }
+    overlayHide() {
+        this.overlay.style.display = "none";
     }
 }
 exports.default = DropHandler;
