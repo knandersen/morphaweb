15ec1772ec2e82952f52f6d4373579fd13e766a3
Kevin Nørby Andersen <things@kevinandersen.dk>
2023-03-30 12:32:44 +0200

    - add helpful markers variable - change all references to use new helpful markers variable - fixes #12 by implementing correct selected marker retrieval functions

--- src/MarkerHandler.js
+++ src/MarkerHandler.js
@@ -5,6 +5,7 @@ export default class MarkerHandler {
         this.color = '#ff990a'
         this.colorHighlight = "#ff1111"
         this.colorTopMarker = "#ff00bb"
+        this.markers = morphaweb.wavesurfer.markers;
 
         this.morphaweb = morphaweb
         this.morphaweb.wavesurfer.on('marker-click',this.onClick.bind(this))
@@ -16,32 +17,47 @@ export default class MarkerHandler {
         }
     }
     removeTopMarker() {
-        const i = this.morphaweb.wavesurfer.markers.markers.map(m => m.position).indexOf('top')
+        const i = this.markers.markers.map(m => m.position).indexOf('top')
         this.morphaweb.wavesurfer.markers.remove(i)
     }
 
     removeSelectedMarker() {
-        const sel = this.morphaweb.wavesurfer.markers.selectedMarker
-        const i = this.getMarkerAtTime(sel.time)
-        this.morphaweb.wavesurfer.markers.remove(i)
+        const i = this.getSelectedMarkerIndex()
+        this.markers.remove(i)
     }
 
     highlightMarker(time) {
-        this.morphaweb.wavesurfer.markers.markers.map(m => {
+        this.markers.markers.map(m => {
             if(m.position=="bottom"){
                 if(m.time == time) {
-                    m.el.children[1].children[0].children[0].setAttribute('fill',this.colorHighlight)
+                    m.el.children[1].children[0].children[0].setAttribute('fill', this.colorHighlight)
                 } else {
-                    m.el.children[1].children[0].children[0].setAttribute('fill',this.color)
+                    m.el.children[1].children[0].children[0].setAttribute('fill', this.color)
                 }       
             }
         })
     }
 
+    getBottomMarkers() {
+        return this.markers.markers.filter(m => m.position === "bottom")
+    }
+
+    getTopMarkers() {
+        return this.markers.markers.filter(m => m.position === "top")
+    }
+
+    getSelectedMarker() {
+        const selectedMarker = this.markers.markers.filter(m => m.el.children[1].children[0].children[0].getAttribute('fill') === this.colorHighlight)[0]
+        return selectedMarker;
+    }
+    getSelectedMarkerIndex() {
+        const selectedMarker = this.getSelectedMarker()
+        return this.markers.markers.findIndex(m => m === selectedMarker)
+    }
+
     getMarkerAtTime(time) {
-        return this.morphaweb.wavesurfer.markers.markers.map(m => {
-            if(m.position == "bottom") { return m.time }
-        }).indexOf(time)
+        const bottomMarkers = this.getBottomMarkers()
+        return bottomMarkers.indexOf(time)
     }
     
     createMarker(time,type="bottom") {
